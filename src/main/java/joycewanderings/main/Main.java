224e313e010ecaed62d8160c9f5daca42bded69c
Kyle Webster <kyle1234webster@gmail.com>
2018-11-19 15:21:03 -0700

    Added events

--- src/main/java/joycewanderings/main/Main.java
+++ src/main/java/joycewanderings/main/Main.java
@@ -1,11 +1,12 @@
 package main.java.joycewanderings.main;
 
-import main.java.joycewanderings.main.game.main.GameGUI;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        GameGUI game = new GameGUI();
+    public static void main(String args[]) {
+
     }
 }
--- src/main/java/joycewanderings/main/filemanager/Image.java
+++
@@ -1,7 +0,0 @@
-package main.java.joycewanderings.main.filemanager;
-
-/**
- * Created by kylewebster on 11/18/18.
- */
-public class Image {
-}
