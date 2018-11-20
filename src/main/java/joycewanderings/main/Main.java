e37d3de70a117e30f5108b0123fd6db6a7322be5
Kyle Webster <kyle1234webster@gmail.com>
2018-11-20 10:47:24 -0700

    FileManager now accepts any directory and will output the files.

--- src/main/java/joycewanderings/main/Main.java
+++ src/main/java/joycewanderings/main/Main.java
@@ -6,7 +6,9 @@ import java.awt.*;
 
 public class Main {
 
-    public static void main(String args[]) {
-
+    public static void main(String args[])throws Exception {
+        FileManager file = new FileManager("/Users/kylewebster/Desktop/test.txt", "username");
+        Event e = new Event(file);
+        System.out.println(e.getContent());
     }
 }
